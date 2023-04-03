138a0525b0acad4d48e31211a80290c312658cc0
damilolajohn6 <damilolajohn622@gmail.com>
2023-04-03 16:19:27 -0700

    second sermon

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-"""
-This module is composed by a class that defines a Rectangle
+"""Module 1-rectangle
+Defines a Rectangle class.
 """
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = height
 
     @property
     def width(self):
