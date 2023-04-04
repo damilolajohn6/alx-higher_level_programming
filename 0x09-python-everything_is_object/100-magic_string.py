5a29114f38d166dee1629a1984fbfcfa077c6f05
damilolajohn6 <damilolajohn622@gmail.com>
2023-04-04 09:33:31 -0700

    everything obj

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
-def magic_string(i=0):
-    i += 1
-    return "BestSchool, ".join(["BestSchool"] * i)
+!/usr/bin/python3
+def magic_string(n=[0]):
+    n[0] += 1
+    return str("BestSchool, " * (n[0] - 1)) + "BestSchool"
