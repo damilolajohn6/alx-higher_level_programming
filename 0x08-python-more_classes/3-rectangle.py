1eaafc6a26d5f9922a253065fff61ac2e47c7b66
damilolajohn6 <damilolajohn622@gmail.com>
2023-04-03 17:06:49 -0700

    rectified

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -5,11 +5,9 @@ Module contains Rectangle Class
 
 class Rectangle:
     """A class defining a rectangle."""
-    
     def __init__(self, width=0, height=0):
         """
         Initializes a rectangle instance.
-        
         Parameters
         ----------
         width : int
@@ -19,22 +17,18 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-    
     @property
     def width(self):
         """Getter for width attribute."""
         return self.__width
-    
     @width.setter
     def width(self, value):
         """
         Setter for width attribute.
-        
         Parameters
         ----------
         value : int
             The new value for the width attribute.
-        
         Raises
         ------
         TypeError
@@ -48,22 +42,18 @@ class Rectangle:
             raise ValueError("Width must not be negative.")
         else:
             self.__width = value
-    
     @property
     def height(self):
         """Getter for height attribute."""
         return self.__height
-    
     @height.setter
     def height(self, value):
         """
         Setter for height attribute.
-        
         Parameters
         ----------
         value : int
             The new value for the height attribute.
-        
         Raises
         ------
         TypeError
@@ -77,22 +67,18 @@ class Rectangle:
             raise ValueError("Height must not be negative.")
         else:
             self.__height = value
-    
     def area(self):
         """
         Calculates the area of the rectangle.
-        
         Returns
         -------
         int
             The area of the rectangle.
         """
         return self.__width * self.__height
-    
     def perimeter(self):
         """
         Calculates the perimeter of the rectangle.
-        
         Returns
         -------
         int
@@ -102,11 +88,9 @@ class Rectangle:
             return 0
         else:
             return 2 * (self.__width + self.__height)
-    
     def __str__(self):
         """
         Returns a graphical representation of the rectangle.
-        
         Returns
         -------
         str
