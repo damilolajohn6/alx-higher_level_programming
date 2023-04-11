08de4d9dece25fceacccc241dc5031c996b3582c
damilolajohn6 <damilolajohn622@gmail.com>
2023-04-11 12:49:53 -0700

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module contains empty class
+'''
+
+class BaseGeometry:
+    '''
+    An empty class BaseGeometry
+    '''
+
+    def area(self):
+        '''
+        Empty area method
+        '''
+        raise Exception('area() is not implemented')
