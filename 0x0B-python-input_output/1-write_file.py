e285914f541e70d759185bfdcc9acdb49a87fbdd
damilolajohn6 <damilolajohn622@gmail.com>
2023-04-12 23:18:46 -0700

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module contains a function for writing to a text file
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Args:
+        filename (string): The name of the file to write to
+        text (string): The text to add to the file
+    '''
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
