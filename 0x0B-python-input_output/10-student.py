b0e49ffd409da4badbdd680dcabec489fbf39bc9
damilolajohn6 <damilolajohn622@gmail.com>
2023-04-12 23:56:04 -0700

    student filter'

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
-
-
+'''
+Module contains a class Student
+'''
 class Student:
-    """Represent a student."""
-
+    '''
+    Defines a Student Attributes:
+        first_name (str): The first name of the student
+        last_name (str): The last name of the student
+        age (int): The age of the student
+    '''
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-       '''
+        '''
         Returns a dictionary representation of a Student instance
-        Args:
+        Arguement:
             attrs (list): A list of required attributes
         '''
-       
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        new_dict = {}
+        if attrs is not None:
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    new_dict[k] = v
+            return new_dict
+        else:
+            return self.__dict__
