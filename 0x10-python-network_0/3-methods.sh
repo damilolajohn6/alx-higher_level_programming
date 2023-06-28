de5d9a5f22791f70841fe3fbd4ad15419c1863e0
damilolajohn6 <damilolajohn622@gmail.com>
2023-06-28 15:23:00 -0700

    header

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
-  echo "Error: Please provide a URL as an argument."
-  exit 1
-fi
-
-allow_methods=$(curl -sI "$1" | awk -F ": " '/Allow/ {print $2}' | tr -d '\r')
-
-if [ -z "$allow_methods" ]; then
-  echo "Error: Unable to retrieve the supported HTTP methods."
-  exit 1
-fi
-
-echo "Supported HTTP Methods: $allow_methods"
+# curl to display all HTTP methods the server accepts
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
